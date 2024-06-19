abee0d3c29f1ac197c6e5da16d20d6da93c57eff
leiming <leiming@janezt.com>
2024-06-19 16:43:05 +0800

    feat: fetch data in Next.js

--- app/page.tsx
+++ app/page.tsx
@@ -17,13 +17,13 @@ export default function Page() {
           <div
             className={styles.shape}
           />
-          <p className={`${lusitana.className} antialiased text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <div className={`${lusitana.className} antialiased text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
-          </p>
+          </div>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
